54fccc11b340484b015826e3d91f61800562e71e
AshishSharma1221 <ashishsharmaashish97@gmail.com>
2024-04-09 10:04:50 -0400

    Product deletion is also implemented. CRUD: done

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Home Page</title>
 </head>
 
 <body>
@@ -13,11 +13,14 @@
 
     <div>
         @if (@session()->has('success'))
-            <div>{{ session('success') }}</div>
+            <div style="color: green">{{ session('success') }}</div>
         @endif
     </div>
-    
+
     <div>
+    <div style="margin-bottom: 20px;">
+        <a href="{{route('product.create')}}">Add New Product</a>
+    </div>
         <table border="1">
             <tr>
                 <th>Id</th>
@@ -26,6 +29,7 @@
                 <th>Quantity</th>
                 <th>Price</th>
                 <th>Edit</th>
+                <th>Delete</th>
             </tr>
             @foreach ($products as $product)
                 <tr>
@@ -35,6 +39,13 @@
                     <td>{{ $product->quantity }}</td>
                     <td>{{ $product->price }}</td>
                     <td><a href="{{ route('product.edit', ['product' => $product]) }}">Edit</a></td>
+                    <td>
+                        <form method="POST" action="{{route('product.destroy', ['product'=>$product])}}">
+                            @csrf
+                            @method('delete')
+                            <input type="submit" value="Delete"/>
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         </table>
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,4 @@ Route::get('/product/create', [ProductController::class, 'create']) -> name('pro
 Route::post('/product', [ProductController::class, 'storeData']) -> name('product.store');
 Route::get('/product/{product}/edit', [ProductController::class, 'edit']) -> name('product.edit');
 Route::put('/product/{product}/update', [ProductController::class, 'update']) -> name('product.update');
+Route::delete('/product/{product}/destroy', [ProductController::class, 'destroy']) -> name('product.destroy');
